27d200e36710d25f58817a8f7858ccf53e6ffef8
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2022-08-03 14:12:11 +1000

    Completed Styling of Litter Management page, moving on to completeing litter creation

--- src/Components/Pages/Litter.js
+++ src/Components/Pages/Litter.js
@@ -1,24 +1,104 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Button, Container, Grid, Paper, Typography, Stack, Table, TableBody, TableCell, TableRow, Collapse, ClickAwayListener, ButtonBase, IconButton, Box } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import { Link } from "react-router-dom";
-import { LitterDetails } from '../utils/';
+import moment from "moment";
+import { LitterDetails, useGlobalState } from '../utils/';
+import { useState } from "react";
 
 const Litter = (props) => {
-  const { litter } = props;
+  const { litter, breeder, sire, bitch } = props;
+  const [open, setOpen] = useState(false);
 
-  console.log(litter);
+  let Breeder = {};
+  let Sire = {};
+  let Bitch = {};
+
+  breeder.forEach(user => {
+    if (user[1].id === litter.breeder_id) {
+      Breeder = user[1];
+    }
+  });
+  sire.forEach(sire => {
+    if (sire[1].id === litter.sire_id) {
+      Sire = sire[1];
+    }
+  });
+  bitch.forEach(bitch => {
+    if (bitch[1].id === litter.bitch_id) {
+      Bitch = bitch[1];
+    }
+  });
 
   return (
     <>
-      <Container>
-        <Grid container>
-          <Grid item>
-            <Typography>{litter.lname}</Typography>
-          </Grid>
-          <Grid item>
-            <LitterDetails id={litter.id} />
-          </Grid>
-        </Grid>
-      </Container>
+      <TableRow>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>
+          <Typography>{litter.lname}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography>{Breeder.username}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography>{Sire.realname}</Typography>
+        </TableCell>
+        <TableCell>
+          <Typography>{Bitch.realname}</Typography>
+        </TableCell>
+        <TableCell>
+          {litter.edate ?
+            <Typography>{moment(litter.edate).format('DD-MM-YYYY')}</Typography>
+            :
+            <Typography>Not Provided</Typography>
+          }
+        </TableCell>
+        <TableCell>
+          {litter.adate ?
+            <Typography>{moment(litter.adate).format('DD-MM-YYYY')}</Typography>
+            :
+            <Typography>Not Provided</Typography>
+          }
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell sx={{ pb: 0, pt: 0 }} colSpan={7}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ m: 1 }}>
+              <Table size="small">
+                <TableBody>
+                  <TableRow sx={{ display: 'flex' }}>
+                    <TableCell align="left" size="small" >
+                      <Button variant="contained" color="success">
+                        View
+                      </Button>
+                    </TableCell>
+                    <TableCell align="left" size="small">
+                      <Button variant="contained" color="warning">
+                        Edit
+                      </Button>
+                    </TableCell>
+                    <TableCell align="left" size="small">
+                      <Button variant="contained" color="error">
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+
+          </Collapse>
+        </TableCell>
+      </TableRow>
     </>
   );
 
--- src/Components/Pages/LitterManage.js
+++ src/Components/Pages/LitterManage.js
@@ -1,31 +1,73 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
-import { useGlobalState } from "../utils";
-import { Litter } from "../utils";
+import { Box, Grid, Paper, Typography, Container, Stack, TableContainer, Table, TableHead, TableRow, TableCell, TableBody, IconButton, Collapse, Button } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { useEffect, useState } from "react";
+import { useGlobalState, Litter } from "../utils";
+import { getUsers } from "../services/authServices";
 
 const LitterManage = () => {
-  const { store } = useGlobalState();
-  const { litterList } = store;
+  const { store, dispatch } = useGlobalState();
+  const { litterList, userList, dogList } = store;
+
+  useEffect(() => {
+    getUsers()
+      .then(users => {
+        dispatch({
+          type: "setUserList",
+          data: users
+        });
+      })
+      .catch(e => console.log(e));
+  }, []);
+
+  const females = Object.entries(dogList).filter(dog => dog[1].sex = 2);
+  const males = Object.entries(dogList).filter(dog => dog[1].sex = 1);
+  const breeder = Object.entries(userList).filter(user => user[1].breeder = true);
 
   return (
     <>
-      <Box sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}>
-        <Paper sx={{ padding: 4 }}>
-          <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-            <Grid item xs={12} sx={{ mt: 3 }}>
-              <Typography variant="h5" component="h1" sx={{ textAlign: "center" }}>Manage Litters</Typography>
-            </Grid>
-            {Object.entries(litterList).map((litter, id) =>
-              <Grid item xs={12} key={id}>
-                <Litter litter={litter[1]} />
-              </Grid>
-            )}
-          </Grid>
-        </Paper>
-      </Box>
+      <Paper sx={{ display: 'flex' }}>
+        <Container sx={{ justifyContent: 'center', textAlign: "center", mt: 4 }}>
+          <Typography variant="h5" component="h1">Manage Litters</Typography>
+          <TableContainer component={Paper}>
+            <Table >
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell align='center'>
+                    <Typography>Litter Name</Typography>
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Typography>Breeder</Typography>
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Typography>Sire</Typography>
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Typography>Bitch</Typography>
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Typography>Expected Date</Typography>
+                  </TableCell>
+                  <TableCell align='center'>
+                    <Typography>Delivered Date</Typography>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.entries(litterList).map((litter, id) =>
+                  <Litter key={id} litter={litter[1]} breeder={breeder} sire={males} bitch={females} />
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Container sx={{ display: "flex", alignContent: "flex-start", p: 2 }}>
+            <Button variant="contained" href="/litters/create">
+              New Litter
+            </Button>
+          </Container>
+        </Container>
+      </Paper>
     </>
   );
 };
