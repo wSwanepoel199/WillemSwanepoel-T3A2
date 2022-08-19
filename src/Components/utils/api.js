eb9129eb7452132d4a2d29704f0f6c82ed9a2589
sjjbirch <birch.jj@gmail.com>
2022-08-19 19:07:53 +1000

    urls updated to point to backend staging deployment

--- src/Components/utils/api.js
+++ src/Components/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const backEndAPI = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://myshalair-back.herokuapp.com/'
 });
 
 backEndAPI.interceptors.request.use(req => {
